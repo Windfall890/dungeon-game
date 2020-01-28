a9bd71876e811d858edfe2999d07c74108fece04
Jon Sanders <Jon.Sanders@redbox.com>
2020-01-27 22:40:12 -0600

    add  explanation for launcher hack

--- src/main/java/com/jsaop/dungeonGame/gui/Launcher.java
+++ src/main/java/com/jsaop/dungeonGame/gui/Launcher.java
@@ -1,5 +1,7 @@
 package com.jsaop.dungeonGame.gui;
 
+//WTF this is a hack to make JFX dependencies load properly
+//see http://mail.openjdk.java.net/pipermail/openjfx-dev/2018-June/021977.html
 public class Launcher {
     public static void main(String[] args) {
         App.main(args);
